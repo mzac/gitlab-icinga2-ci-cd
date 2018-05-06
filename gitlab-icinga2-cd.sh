42c5108eb82a571dba1691887df3b8fdbff85f64
mzac <zachary.mcgibbon@gmail.com>
2018-05-06 12:41:04 -0400

    Update gitlab-icinga2-cd.sh

--- gitlab-icinga2-cd.sh
+++ gitlab-icinga2-cd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd /etc/icinga2/conf.d/private
-git pull
-systemctl reload icinga2
+
+export GIT_OUTPUT=`git pull`
+
+if [[ $GIT_OUTPUT = *".conf"* ]]; then
+  echo "Changes made, reloading Icinga2"
+  systemctl reload icinga2
+else
+  echo "No changes made, NOT reloading Icinga2
+fi
